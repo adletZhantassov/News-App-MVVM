a0fbdd514f03a9cb492c18f71215eb539095064a
Adlet <adlet_1996@mail.ru>
2022-10-27 22:37:49 +0600

    Initial Commit

---
+++ News App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by Adlet Zhantassov on 27.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
